3936003bd2b93cb597610c20d65cb22c377ecd9f
YuriDomingos <josemarlabareda@gmail.com>
2021-10-24 07:24:50 +0100

    Subindo a primeira versao do codigo

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
+
+/*
+@Autor : Yuri Domingos
+Data   : 24 - 10 - 2021
+Objectivo : Construir a primeira versao da nossa aplication 
+*/
 import './App.scss';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="Hero">
+      
     </div>
   );
 }
